30563ee2cdc249bbbb2f1e4dc39f8cf2d33108f3
TarunDamarasingi150 <megastarchiru151@gmail.com>
2024-11-14 15:32:29 +0530

    Update DevClasss.java

--- src/main/java/org/example/DevClasss.java
+++ src/main/java/org/example/DevClasss.java
@@ -4,6 +4,7 @@ public class DevClasss {
     public static void main(String[] args) {
         System.out.println("DevClass");
         //Forking Process Checked succesfully !
+        //new changes in Fork according to tutorial
          System.out.println("Forking done via another account");
     }
 }
